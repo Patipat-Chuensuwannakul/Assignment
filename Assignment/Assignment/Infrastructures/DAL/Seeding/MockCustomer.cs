e04a88516826f09a6b983b0c32f1366aa419f577
COMPANY\pchuensuwannakul <pchuensuwannakul@manaosoftware.com>
2019-10-01 11:01:30 +0700

    Add mock for Customer

---
+++ Assignment/Assignment/Infrastructures/DAL/Seeding/MockCustomer.cs
@@ -0,0 +1,25 @@
+﻿using Assignment.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Infrastructures.DAL.Seeding
+{
+    public static class MockCustomer
+    {
+        public static void CreateCustomer(AssignmentDbContext context)
+        {
+            var customer1 = new Customer()
+            {
+                Email = "manaotest@domain.com",
+                Mobile = 0913962985,
+                Name = "Manao Test1",
+                Transactions = new List<Transaction>()
+            };
+
+            context.Add(customer1);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Assignment/Assignment/Infrastructures/DBContextExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Assignment.Infrastructures.DAL;
+using Assignment.Infrastructures.DAL.Seeding;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Linq;
+
+namespace Assignment.Infrastructures
+{
+    public static class DBContextExtensions
+    {
+        public static bool AllMigrationsApplied(this AssignmentDbContext context)
+        {
+            var applied = context.GetService<IHistoryRepository>()
+                .GetAppliedMigrations()
+                .Select(m => m.MigrationId);
+
+            var total = context.GetService<IMigrationsAssembly>()
+                .Migrations
+                .Select(m => m.Key);
+
+            return !total.Except(applied).Any();
+        }
+
+        public static void EnsureSeeded(this AssignmentDbContext context)
+        {
+            MockCustomer.CreateCustomer(context);
+        }
+
+    }
+}
